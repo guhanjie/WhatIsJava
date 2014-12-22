c7f3aa7d0d49f245040fe2ca0955524d8bf3e581
liuxey <liuxey@gmail.com>
2014-12-22 17:15:04 +0800

    初始化导入

--- src/main/java/memory/OOM.java
+++ src/main/java/memory/OOM.java
@@ -1,3 +1,5 @@
+package memory;
+
 /**
  * Created by ShaoJin on 2014/12/19 0019.
  */
@@ -13,7 +15,7 @@ public class OOM {
     public static void nest() {
         length++;
         try {
-            nest();
+            nest(); // 无限循环调用导致调用栈溢出抛出StackOverflowError :]
 
         } catch (Throwable e) {
             System.out.println("StackLength:" + length);
